ae249d414d4716fed3a803538fb2bbe7c551c86f
Dinis <d.yudenka@yandex.by>
2022-01-25 03:59:36 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultWord = '';
+        for(let i = 0; i < expr.length; i+=10) {
+          let word = expr.slice(i, i+10).replace(/(\*){10}/g,' ').replace(/00/g,'').replace(/10/g,'.').replace(/11/g,'-');
+            if (word == ' ') {
+                resultWord += word;
+            } else {
+                resultWord += MORSE_TABLE[word]
+            }
+        }
+    return resultWord
 }
 
 module.exports = {
